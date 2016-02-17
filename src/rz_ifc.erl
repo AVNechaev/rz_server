346e5245485a52c1db808de8b55dab2d8417d752
ANechaev <ANechaev@102new>
2016-02-17 19:48:38 +0300

    robot's external interface

---
+++ src/rz_ifc.erl
@@ -0,0 +1,31 @@
+%%%-------------------------------------------------------------------
+%%% @author user
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 17. Февр. 2016 19:41
+%%%-------------------------------------------------------------------
+-module(rz_ifc).
+-author("user").
+
+%% API
+-export([load_instrs_from_file/1, load_instrs/1]).
+
+-include_lib("iqfeed_client/include/iqfeed_client.hrl").
+
+%%--------------------------------------------------------------------
+-spec load_instrs(Instrs :: [instr_name()]) -> ok.
+load_instrs(Instrs) ->
+  [
+    online_history_worker:set_instrs(online_history_worker:reg_name(N), Instrs)
+    || {N, _} <- iqfeed_util:get_env(rz_server, frames)
+  ],
+  iql1_conn:set_instrs(Instrs),
+  ok.
+
+%%--------------------------------------------------------------------
+-spec load_instrs_from_file(Filename :: file:filename()) -> ok.
+load_instrs_from_file(Filename) ->
+  {ok, I} = iqfeed_util:load_instr_csv(Filename),
+  load_instrs(I).
