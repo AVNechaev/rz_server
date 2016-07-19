164db715d6881e2f260a19ea17ad8f4755d45dd3
ANechaev <anechaev@anthink>
2016-07-19 10:14:48 +0300

    sma store

--- src/sma_store.erl
+++ src/sma_store.erl
@@ -12,7 +12,7 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0, add_daily_candle/1]).
+-export([start_link/0, add_daily_candle/1, get_sma/2]).
 
 %% gen_server callbacks
 -export([init/1,
@@ -26,8 +26,9 @@
 -include_lib("rz_util/include/rz_util.hrl").
 
 -define(SMA_KEY(Instr, Type), {sma, Instr, Type}). %% здесь хранятся последние посчитанные SMA
--define(HISTORY_DATA(Instr, Type), {hist, Instr, Type}). %% здесь хранятся свечки up to depth
--record(state, {}).
+-define(HISTORY_KEY(Instr, Type), {hist, Instr, Type}). %% здесь хранятся свечки up to depth
+-define(STORE_NAME, ets_sma_store).
+-record(state, {known_smas :: [{Type :: atom(), Depth :: pos_integer()}]}).
 
 %%%===================================================================
 %%% API
@@ -40,16 +41,27 @@ start_link() -> gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 add_daily_candle(Candle) -> gen_server:call(?SERVER, {add_daily_candle, Candle}).
 
 %%--------------------------------------------------------------------
--spec get_sma(Instr :: instr_name(), Depth :: atom()).
+-spec get_sma(Instr :: instr_name(), Type :: atom()) -> {ok, Data :: float()} | {error, not_found}.
+get_sma(Instr, Type) ->
+  case ets:lookup(?STORE_NAME, ?SMA_KEY(Instr, Type)) of
+    [] -> {error, not_found};
+    [{_, Data}] -> {ok, Data}
+  end.
 
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
 init([]) ->
-  {ok, #state{}}.
+  ets:new(?STORE_NAME, [protected, named_table, set, {read_concurrency, true}]),
+  SMAConf = rz_util:get_env(rz_server, sma_store),
+  {ok, #state{known_smas = proplists:get_value(SMAConf, known)}}.
 
 %%--------------------------------------------------------------------
-handle_call(_Request, _From, _State) -> exit(handle_call_unsupported).
+handle_call({add_daily_candle, Candle}, _From, State = #state{known_smas = Known}) ->
+  lists:foreach(fun(I) -> process_single(Candle, I) end, Known),
+  {reply, ok, State}.
+
+%%--------------------------------------------------------------------
 handle_cast(_Request, _State) -> exit(handle_cast_unsupported).
 handle_info(_Info, _State) -> exit(handle_info_unsupported).
 
@@ -60,3 +72,35 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+process_single(#candle{name = Instr, close = Data}, {SmaType, Depth}) ->
+  Key = ?HISTORY_KEY(Instr, SmaType),
+  Q =
+    case ets:lookup(?STORE_NAME, Key) of
+      [] ->
+        ets:insert_new(?STORE_NAME, {Key, undefined}), %%затрется при update_element
+        queue:new();
+      [{_, D}] -> D
+    end,
+  NewQ =
+    case queue:len(Q) of
+      L when L == Depth ->
+        {{value, Dropped}, Q1} = queue:out(Q),
+        NQ = queue:in(Data, Q1),
+        recalc_sma(Instr, Dropped, NQ, SmaType),
+        NQ;
+      LM when LM < Depth ->
+        queue:in(Data, Q)
+    end,
+  true = ets:update_element(?STORE_NAME, Key, {2, NewQ}),
+  ok.
+
+%%--------------------------------------------------------------------
+recalc_sma(Instr, Dropped, NQ, SmaType) ->
+  L = queue:len(NQ),
+  Key = ?SMA_KEY(Instr, SmaType),
+  Res =
+    case ets:lookup(?STORE_NAME, Key) of
+      [] -> lists:foldl(fun(D, Acc) -> Acc + D/L end, 0, queue:to_list(NQ));
+      [{_, V}] -> V + (queue:get_r(NQ) - Dropped) / L
+    end,
+  ets:insert(?STORE_NAME, {Key, Res}).
