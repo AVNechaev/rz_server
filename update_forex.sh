5c2b83682ed9ce706542bb7f7ce7bad72c01e7be
an@andell <an@andell>
2018-05-24 00:26:31 +0300

    script copy

---
+++ update_forex.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+set -e
+
+cd rz_server
+escript ./rebar3 as prod release
+scp ./_build/prod/rel/rz_server/lib/rz_server-1/ebin/*.beam RZ:~/rz_forex/dist/lib/rz_server-1/ebin/
+scp ./_build/prod/rel/rz_server/lib/rz_util-1/ebin/*.beam RZ:~/rz_forex/dist/lib/rz_util-1/ebin/
+scp ./_build/prod/rel/rz_server/lib/iqfeed_client-1/ebin/*.beam RZ:~/rz_forex/dist/lib/iqfeed_client-1/ebin/
+scp ./_build/prod/rel/rz_server/lib/erlang_localtime-1.0.1/ebin/*.beam RZ:~/rz_forex/dist/lib/erlang_localtime-1.0.1/ebin/
