554f4e03eff8407f88899dd5cff7b4a4dd4d75ab
an <an@anjaro>
2021-04-01 18:11:45 +0300

    some cleanup

--- update_forex.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-set -e
-
-escript ./rebar3 as prod release
-scp ./_build/prod/rel/rz_server/lib/rz_server-1/ebin/*.beam RZH:~/rz_forex/server/lib/rz_server-1/ebin/
-#scp ./_build/prod/rel/rz_server/lib/rz_util-1/ebin/*.beam RZ:~/rz_forex/dist/lib/rz_util-1/ebin/
-#scp ./_build/prod/rel/rz_server/lib/iqfeed_client-1/ebin/*.beam RZ:~/rz_forex/dist/lib/iqfeed_client-1/ebin/
-#scp ./_build/prod/rel/rz_server/lib/erlang_localtime-1.0.1/ebin/*.beam RZ:~/rz_forex/dist/lib/erlang_localtime-1.0.1/ebin/
